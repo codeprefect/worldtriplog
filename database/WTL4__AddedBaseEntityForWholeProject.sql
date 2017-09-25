a4becd95ddbd2f8339ad6e98973cbe818caeb343
Mubarak Imam <mubarakadeimam@gmail.com>
2017-09-25 12:32:21 +0100

    fixed some repetitions in trip model

---
+++ database/WTL4__AddedBaseEntityForWholeProject.sql
@@ -0,0 +1,25 @@
+﻿DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Trips') AND [c].[name] = N'DateCreated');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Trips] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [Trips] DROP COLUMN [DateCreated];
+
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'Trips') AND [c].[name] = N'UserName');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Trips] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [Trips] DROP COLUMN [UserName];
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20170925104918_ChangesToTripAndStopModels', N'2.0.0-rtm-26452');
+
+GO
+
---
+++ src/WorldTripLog.Web/Migrations/20170925104918_ChangesToTripAndStopModels.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WorldTripLog.Web.Migrations
+{
+    public partial class ChangesToTripAndStopModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Trips");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Trips",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Trips",
+                nullable: true);
+        }
+    }
+}
