162a6e983754f778e30681e12fe531542be7474e
barakimam <mubarakadeimam@gmail.com>
2017-10-07 08:03:26 +0100

    added abstract base entity class

---
+++ src/WorldTripLog.Domain/Entities/Entity.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WorldTripLog.Domain.Interfaces;
+
+namespace WorldTripLog.Domain.Entities
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public T Id { get; set; }
+
+        object IEntity.Id
+        {
+            get => this.Id;
+            set => this.Id = (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        public string Name { get; set; }
+
+        private DateTime? createdDate;
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate
+        {
+            get { return createdDate ?? DateTime.UtcNow; }
+            set { createdDate = value; }
+        }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        [Timestamp]
+        public byte[] Version { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? Deleted { get; set; }
+    }
+}
