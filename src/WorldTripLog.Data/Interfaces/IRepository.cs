0c52d42ff944c4dd27f4632f1608e7d75b4e4d77
barakimam <mubarakadeimam@gmail.com>
2017-10-16 12:24:44 +0100

    added domain access layer as a class library

---
+++ src/WorldTripLog.Data/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+
+using System.Threading.Tasks;
+using WorldTripLog.Domain.Interfaces;
+
+namespace WorldTripLog.Data.Interfaces
+{
+    public interface IRepository<TContext> : IReadOnlyRepository
+    {
+        void Create<TEntity>(TEntity entity, string createdBy = null)
+            where TEntity : class, IEntity;
+
+        void Update<TEntity>(TEntity entity, string modifiedBy = null)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(object id)
+            where TEntity : class, IEntity;
+
+        void Delete<TEntity>(TEntity entity)
+            where TEntity : class, IEntity;
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
